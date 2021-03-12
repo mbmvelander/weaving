97cbf140038e9ecc03293d2667d618f58ee4e884
Malin Velander <malin.velander@spiideo.com>
2021-03-12 08:25:53 +0100

    IMPROVEMENT: calculate weights

--- util/conversions.py
+++ util/conversions.py
@@ -1,2 +1,6 @@
 def cm_to_m(value):
     return value/100.
+
+
+def kg_to_g(value):
+    return value * 1000.
--- weaving_project.py
+++ weaving_project.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 import yaml
+import json
+import re
 from copy import deepcopy
 from collections import defaultdict
-from util.conversions import cm_to_m
+from util.conversions import cm_to_m, kg_to_g
 
 
 DEFAULT_WARP = {
@@ -20,8 +22,8 @@ DEFAULT_WARP = {
     "length": 0.,
     "width": 0.,
     "pattern_ends": 0, # Number of ends in one pattern repeat
-    "yarn": [],
-    "products": [],
+    "yarn": {},
+    "products": {},
     "n_ends": 0,
     "n_pattern_repeats": 0,
     "adjusted_weaving_width": 0.,
@@ -40,6 +42,10 @@ DEFAULT_YARN = {
     "currency_conversion": 1.,
     "url": "",
     "fraction": 1.,
+    "length": 0.,
+    "weight": 0.,
+    "n_ends": 0,
+    "finished_cloth_length": 0.,
 }
 
 
@@ -50,8 +56,9 @@ DEFAULT_PRODUCT = {
     "hems": 0.,
     "fringes": 0.,
     "density": 10.,
-    "yarn": [],
+    "yarn": {},
     "fringe_shortening": 20., # percentage, due to twisting, braiding
+    "weaving_length": 0.,
 }
 
 
@@ -76,16 +83,25 @@ class WeavingProject:
         for y in warp_yarn:
             yarn_data = deepcopy(DEFAULT_YARN)
             yarn_data.update(y)
-            self.out_data["yarn"].append(yarn_data)
+            yarn_id = " ".join([yarn_data["material"], yarn_data["thickness"], yarn_data["colour"]])
+            yarn_id = re.sub(r'[^\w\s]', '-', yarn_id)
+            yarn_id = re.sub(r'\s+', '_', yarn_id)
+            self.out_data["yarn"][yarn_id] = yarn_data
         for p in products:
             weft_yarn = p.pop("yarn")
             product_data = deepcopy(DEFAULT_PRODUCT)
             product_data.update(p)
+            product_id = " ". join([product_data["name"], str(product_data["ordinal"])])
+            product_id = re.sub(r'[^\w\s]', '-', product_id)
+            product_id = re.sub(r'\s+', '_', product_id)
             for y in weft_yarn:
                 yarn_data = deepcopy(DEFAULT_YARN)
                 yarn_data.update(y)
-                product_data["yarn"].append(yarn_data)
-            self.out_data["products"].append(product_data)
+                yarn_id = " ".join([yarn_data["material"], yarn_data["thickness"], yarn_data["colour"]])
+                yarn_id = re.sub(r'[^\w\s]', '-', yarn_id)
+                yarn_id = re.sub(r'\s+', '_', yarn_id)
+                product_data["yarn"][yarn_id] = yarn_data
+            self.out_data["products"][product_id] = product_data
     
     def load_dump_file(self, filepath):
         with open(filepath, 'r') as f:
@@ -102,8 +118,16 @@ class WeavingProject:
             return
         self.out_data[key] = value
     
-    def warp_length(self):
-        if self.out_data["length"]:
+    def weaving_length(self, product, refresh=False):
+        if product["weaving_length"] and not refresh:
+            return product["weaving_length"]
+
+        shrinkage = 1. + self.out_data["shrinkage"]["length"] / 100.
+        product["weaving_length"] = (product["length"] + product["hems"]) * shrinkage
+        return product["weaving_length"]
+    
+    def warp_length(self, refresh=False):
+        if self.out_data["length"] and not refresh:
             return self.out_data["length"]
 
         total_length = 0.
@@ -111,15 +135,13 @@ class WeavingProject:
         total_length += self.out_data["evening_weaving"]
         total_length += self.out_data["tying"]
         total_length += self.out_data["efsingar"]
-        shrinkage = 1. + self.out_data["shrinkage"]["length"] / 100.
-        for p in self.out_data["products"]:
-            weaving_length = (p["length"] + p["hems"]) * shrinkage
-            total_length += weaving_length + p["fringes"]
+        for p in self.out_data["products"].values():
+            total_length += self.weaving_length(p, refresh=refresh) + p["fringes"]
         self.out_data["length"] = total_length
         return total_length
 
-    def n_ends(self):
-        if self.out_data["n_ends"]:
+    def n_ends(self, with_extras=True, refresh=False):
+        if self.out_data["n_ends"] and not refresh:
             return self.out_data["n_ends"]
 
         shrinkage = 1. + self.out_data["shrinkage"]["width"] / 100.
@@ -132,11 +154,89 @@ class WeavingProject:
         n_ends_whole_patterns = n_pattern_repeats * pattern_ends
         n_ends_total = n_ends_whole_patterns + self.out_data["extra_ends"]
         self.out_data["n_ends"] = n_ends_total
+        self.out_data["n_ends_whole_patterns"] = n_ends_whole_patterns
         self.out_data["n_pattern_repeats"] = n_pattern_repeats
-        self.out_data["adjusted_weaving_width"] = n_ends_whole_patterns / density
-        self.out_data["adjusted_final_width"] = self.out_data["adjusted_weaving_width"] / shrinkage
-        return n_ends_total
+        if with_extras:
+            return n_ends_total
+        else:
+            return n_ends_whole_patterns
+    
+    def weaving_width(self, final=False, refresh=False):
+        if self.out_data["adjusted_weaving_width"] and not refresh:
+            return self.out_data["adjusted_weaving_width"]
 
+        shrinkage = 1. + self.out_data["shrinkage"]["width"] / 100.
+        self.out_data["adjusted_weaving_width"] = self.n_ends(with_extras=False) / self.out_data["density"]
+        self.out_data["adjusted_final_width"] = self.out_data["adjusted_weaving_width"] / shrinkage
+        if final:
+            return self.out_data["adjusted_final_width"]
+        else:
+            return self.out_data["adjusted_weaving_width"]
+    
+    def warp_length_all_ends(self, n_ends=None, refresh=False):
+        if n_ends is None:
+            n_ends = self.n_ends(refresh=refresh)
+
+        total_length = self.warp_length(refresh=refresh) * n_ends
+        return total_length
+    
+    def _yarn_weight_warp(self, yarn, n_ends=None, refresh=False):
+        if yarn["weight"] and not refresh:
+            return yarn["weight"]
+        
+        if n_ends is None:
+            n_ends = round(self.n_ends(refresh=refresh)*yarn["fraction"])
+        
+        yarn["n_ends"] = n_ends
+        yarn["length"] = self.warp_length_all_ends(n_ends=yarn["n_ends"], refresh=refresh)
+        yarn["weight"] = kg_to_g((cm_to_m(yarn["length"]) / yarn["m_per_kg"]))
+        yarn["weight"] /= (1 - yarn["m_per_kg_error"] / 100.) # Increase weight by error factor so as not to underestimate how much to buy
+        return yarn["weight"]
+    
+    def _yarn_weight_weft(self, product, yarn, finished_cloth_length=None, refresh=False):
+        if yarn["weight"] and not refresh:
+            return yarn["weight"]
+        
+        if finished_cloth_length is None:
+            finished_cloth_length = self.weaving_length(product, refresh=refresh) * yarn["fraction"]
+        
+        yarn["finished_cloth_length"] = finished_cloth_length
+        yarn["length"] = product["density"] * finished_cloth_length * self.weaving_width(refresh=refresh)
+        yarn["weight"] = kg_to_g((cm_to_m(yarn["length"]) / yarn["m_per_kg"]))
+        yarn["weight"] /= (1 - yarn["m_per_kg_error"] / 100.) # Increase weight by error factor so as not to underestimate how much to buy
+        return yarn["weight"]
+    
+    def yarn_weights(self, refresh=False):
+        return_data = {"warp": {}, "weft": {}}
+
+        # Warp yarns
+        ends_used = 0
+        weight_total = 0.
+        yarns = [i for i in self.out_data["yarn"].items()]
+        for yarn_id, yarn in yarns[:-1]:
+            w = self._yarn_weight_warp(yarn, refresh=refresh)
+            return_data["warp"][yarn_id] = w
+            weight_total += w
+            ends_used += yarn["n_ends"]
+        yarn_id, yarn = yarns[-1]
+        w = self._yarn_weight_warp(yarn, n_ends=self.n_ends(refresh=refresh) - ends_used, refresh=refresh)
+        return_data["warp"][yarn_id] = w
+        weight_total += w
+        return_data["warp"]["total"] = weight_total
+
+        # Weft yarns
+        for product_id, product in self.out_data["products"].items():
+            weight_total = 0.
+            return_data["weft"][product_id] = {}
+            for yarn_id, yarn in product["yarn"].items():
+                w = self._yarn_weight_weft(product, yarn, refresh=refresh)
+                return_data["weft"][product_id][yarn_id] = w
+                weight_total += w
+                ends_used += yarn["n_ends"]
+        return return_data
+
+    def warp_total_weight(self, n_ends=None):
+        return
 
 
 if __name__ == "__main__":
@@ -144,3 +244,4 @@ if __name__ == "__main__":
     project.load_input_file("/Users/malin/src/github.com/mbmvelander/weaving/pebbles.yml")
     print(cm_to_m(project.warp_length()))
     print(project.n_ends())
+    print(json.dumps(project.yarn_weights(), indent=4))
